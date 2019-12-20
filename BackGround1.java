82606079c0c2a385f929178de9aed6d268933a59
Haashim Rehan <haashimr@hotmail.com>
2019-12-20 15:31:59 -0500

    Added Usman's Code with different sprites

--- BackGround1.java
+++ BackGround1.java
@@ -5,7 +5,6 @@ import greenfoot.*;
  * 
  * @author (your name) 
  * @version (a version number or a date)
- * Testing
  */
 public class BackGround1 extends World
 {
@@ -16,26 +15,19 @@ public class BackGround1 extends World
      */
     public BackGround1()
     {    
-        super(1500, 1000, 1); 
-        addObject(new DK(), 200, 150);
+        super(800, 800, 1); 
+        addObject(new DK(), 150, 150);
         addObject(new Floor(), 125, 219);
         addObject(new Floor(), 375, 219);
-        addObject(new Floor(), 625, 219);
-        addObject(new Floor(), 875, 219);
         addObject(new Floor2(), 625, 500);
         addObject(new Floor2(), 875, 500);
-        addObject(new Floor2(), 1125, 500);
-        addObject(new Floor2(), 1375, 500);
+      
+        
         addObject(new Floor(), 125, 781);
         addObject(new Floor(), 375, 781);
         addObject(new Floor(), 625, 781);
         addObject(new Floor(), 875, 781);
-        addObject(new Floor2(), 125, 1000);
-        addObject(new Floor2(), 375, 1000);
-        addObject(new Floor2(), 625, 1000);
-        addObject(new Floor2(), 875, 1000);
-        addObject(new Floor2(), 1125, 1000);
-        addObject(new Floor2(), 1375, 1000);
-        addObject(new Mario(), 125, 940);
+
+        addObject(new Mario(), 125, 700);
     }
 }

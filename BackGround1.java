8fef5ddd1c1f1637b1b40aa50b3d4f0587cd32c3
RadiatedSilver <55992433+RadiatedSilver@users.noreply.github.com>
2020-01-07 14:44:24 -0500

    Added a coin spawner
    - I organized the code  in background
    - Added in a random coin generator using pseudo random function in this
    - created a coin class which I will be used to add animation and score
    - resized the coin size like 1000 times to finally get a decent sized coin.

--- BackGround1.java
+++ BackGround1.java
@@ -26,14 +26,23 @@ public class BackGround1 extends World
         addObject(new Floor(), 375, 219);
         addObject(new Floor2(), 625, 500);
         addObject(new Floor2(), 875, 500);
-      
         addObject(new Floor(), 125, 781);
         addObject(new Floor(), 375, 781);
         addObject(new Floor(), 625, 781);
         addObject(new Floor(), 875, 781);
-
         addObject(new Mario(), 125, 700);
+        
         GreenfootSound backgroundMusic = new GreenfootSound("BackgroundMusic.mp3");
         backgroundMusic.playLoop();
+        
+        double[][] screenCoinGrid = new double[9][9];
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+            screenCoinGrid[i][i] = ((int)(Math.random() * (20 + 1)));
+            if(screenCoinGrid[i][i] == 1) {
+                addObject(new Coin(), 50+ i*75, 50 + j*75);
+            }
+        }
+        }
     }
 }

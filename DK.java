8e82d042ca6815a729d6206c65940eae30ae253e
DeanMousa <34653850+DeanMousa@users.noreply.github.com>
2020-01-09 14:46:57 -0500

    trying and improved donket kong animation.

--- DK.java
+++ DK.java
@@ -1,19 +1,44 @@
 import greenfoot.*;
 public class DK extends Actor
 {
+    boolean changeImage = false;
+    long frameCount = 0;
+    String[] imageNames = {"DKRightHand.png","DKLeftHand.png","DKIdol.png","DKAngryIdol.png" };  
     long lastTime;
     public void act() 
     {
+        frameCount++;
+
+                
         if(isTouching(Mario.class))
         {
             Greenfoot.setWorld(new Finish());
 
             Greenfoot.stop();
         }
-        if(System.currentTimeMillis() - lastTime > 2500)
+        /*if(System.currentTimeMillis() - lastTime > 2500)
         {
+
+        }
+         */
+        if (frameCount %150 ==0) {
+
             lastTime = System.currentTimeMillis();
             getWorld().addObject(new Barrel(), getX(), getY());
         }
-    }    
+
+                
+        if (frameCount %75 ==0) {
+            changeImage = !changeImage;
+
+        }
+        if(changeImage) {
+            setImage(imageNames[2]);
+
+        }         else {
+            setImage(imageNames[3]);
+        }
+
+    } 
 }
+
